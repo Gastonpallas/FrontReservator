9e773e52755454e987618106eecad7e167830fa2
Gaston Pallas <pallasgaston@gmail.com>
2023-05-15 08:50:57 +0200

    Page d'accueil et menu

---
+++ src/components/Accueil.tsx
@@ -0,0 +1,18 @@
+
+function Accueil() {
+
+
+    return (
+
+        <div className="flex items-center justify-center h-screen">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-4">Bienvenue</h1>
+                <p className="text-lg">Réservez votre salle dès maintenant !</p>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default Accueil;
